aaf73062e93844bc9f4a9f705db4693aa0cc64ae
skitsanos <info@skitsanos.com>
2021-06-11 12:28:28 +0300

    Core functionality

---
+++ mock/locales.js
@@ -0,0 +1,14 @@
+export default {
+    'GET /api/locales': {
+        'en-US': {
+            'FOOTER': 'Skitsanos © 2021',
+            'SIMPLE': 'Simple Sentence',
+            'HELLO': 'Hello, {name}. Welcome to {where}!'
+        },
+
+        'ru': {
+            'FOOTER': 'Skitsanos © 2021. Все права защищены',
+            'HELLO': 'Привет, {name}. Добро пожаловать в {where}!'
+        }
+    }
+};
